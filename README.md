1bdc19334f05d7a4868fb3d67396a9b34f742487
Alex <alar0@yahoo.com>
2024-10-27 22:34:57 +0600

    begin to fill README.md, add .npmignore

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # cleaning-objects
+This package allows you to recursively clean objects from empty keys (NULL values, empty objects, strings and arrays), filter objects using a callback function.
+## Install
+```
+npm install cleaning-objects
+```
+## Usage
+### filterObject
+First year of copyright is required.
+```javascript
+filterObject(obj, callback);
+```
+In this example, if the current year is 2020 or earlier than the first year, the output will be:
+```
+©2020.
+```
+### removeEmptyObjects
+```javascript
+removeEmptyObjects(obj);
+```
+In this example, if the current year is 2020 or earlier than the first year, the output will be:
+```
+©2020.
+```
+### clearObject & cleanObject
+
+```javascript
+clearObject(obj);
+cleanObject(obj);
+```
+In this example, if the current year is 2020 or earlier than the first year, the output will be:
+
+```javascript
+cleanObject(obj, [`array`, `string`, `null`, `undefined`, `NaN`, `Infinity`, 'emptyObject', 'embeddedObject']);
+```
+In this example, if the current year is 2020 or earlier than the first year, the output will be:
+```
+©2020.
+```
--- tests/cleanObject.test.ts
+++ tests/cleanObject.test.ts
@@ -1,4 +1,4 @@
-import { clearObject } from '../src';
+import { cleanObject } from '../src';
 
 const obj = {
     a: 1,
@@ -30,7 +30,7 @@ const obj = {
 }
 
 test('clear all empty values in the object', () => {
-    const result = clearObject(obj);
+    const result = cleanObject(obj);
     const testObj = {
         a: 1,
         b: "2",
@@ -44,6 +44,180 @@ test('clear all empty values in the object', () => {
             d: false,
             e: [5],
         }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except array', () => {
+    const result = cleanObject(obj, ['array']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        f: [],
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            f: [],
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except strings', () => {
+    const result = cleanObject(obj, ['string']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        g: "",
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            g: "",
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except null', () => {
+    const result = cleanObject(obj, ['null']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        h: null,
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            h: null,
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except undefined', () => {
+    const result = cleanObject(obj, ['undefined']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        i: undefined,
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            i: undefined,
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except NaN', () => {
+    const result = cleanObject(obj, ['NaN']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        j: NaN,
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            j: NaN,
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except Infinity', () => {
+    const result = cleanObject(obj, ['Infinity']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        k: Infinity,
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            k: Infinity,
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except empty object', () => {
+    const result = cleanObject(obj, ['emptyObject']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        l: {},
+        m: {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            l: {},
+        }
+    }
+    expect(result).toStrictEqual(testObj);
+});
+
+test('clear all empty values in the object except embedded object', () => {
+    const result = cleanObject(obj, ['embeddedObject']);
+    const testObj = {
+        a: 1,
+        b: "2",
+        c: true,
+        d: false,
+        e: [5],
+        l: {},
+        m:  {
+            a: 1,
+            b: "2",
+            c: true,
+            d: false,
+            e: [5],
+            f: [],
+            g: "",
+            h: null,
+            i: undefined,
+            j: NaN,
+            k: Infinity,
+            l: {},
+        },
     }
     expect(result).toStrictEqual(testObj);
 });
